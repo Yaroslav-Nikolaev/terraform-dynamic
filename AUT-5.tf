b0a23561720347cd4aea7adf5fcd624d4d959acb
Service User <service_account@optiva.com>
2019-11-07 18:39:41 +0300

    AUT-5.tf

--- AUT-5.tf
+++ AUT-5.tf
@@ -29,7 +29,7 @@ resource "google_compute_instance" "default" {
     product = "uc",
     type = "aio",
     end_of_life = "1574380800",
-    working_till = "1573141124",
+    working_till = "1573141240",
     jira="aut-5"
   }
 
